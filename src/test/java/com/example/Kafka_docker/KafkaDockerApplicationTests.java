8a1637c5eba8fd143d7b232198d0fd159633b7fa
apurba <apurbakumar.3110@gmail.com>
2024-11-24 20:16:22 +0530

    Initial commit

---
+++ src/test/java/com/example/Kafka_docker/KafkaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kafka_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
